4187f014ee479b373502d55dc0c56074bea50675
Dominik Künkele <dominik.kuenkele@outlook.com>
2024-01-31 17:07:10 +0100

    empires section

--- src/data.tsx
+++ src/data.tsx
@@ -125,6 +125,13 @@ export const projects: Array<Item> = [
             <p><strong>available on <a href="https://github.com/DominikKuenkele/MLT_Dialogue-Systems_empires" target="_blank" rel="noreferrer"> Github <span className="material-symbols-outlined">open_in_new</span></a></strong></p>
             <p>I have developed a strategy game which the player can control through an interactive dialogue with a chatbot. Hereby, the player can use natural language to express their moves while the game can respond, clarify or execute the proper action with variability. The whole game is implemented in React, using the XState framework to control the game flow. Voice is transcribed and generated through Microsoft Azure services and subsequently parsed with the help of Rasa.
             </p>
+            <h3>Code</h3>
+            <a href="https://github.com/DominikKuenkele/MLT_Dialogue-Systems_empires" target="_blank" rel="noreferrer">
+                Game <span className="material-symbols-outlined">open_in_new</span>
+            </a><br />
+            <a href='https://github.com/DominikKuenkele/MLT_Dialogue-Systems_empires-rasa' target="_blank" rel="noreferrer">
+                Rasa server <span className="material-symbols-outlined">open_in_new</span>
+            </a>
         </div>
     },
     {
